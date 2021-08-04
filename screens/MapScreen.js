e6fd749825169f8ac478a55841ebf7dec40fce9e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-08-04 00:34:55 -0500

    install react navigator

---
+++ screens/MapScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is MapScreen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
