24eb48d497636f1176cdc09a567586990a762c0d
estherkimyunjung <estherkimyunjung@gmail.com>
2021-08-11 23:55:09 -0500

    updated maker

--- components/Map.js
+++ components/Map.js
@@ -24,6 +24,9 @@ export default function Map() {
             latitude: origin.location.lat,
             longitude: origin.location.lng,
           }}
+          title="Origin"
+          description={origin.description}
+          identifier="origin"
         />
       )}
     </MapView>
