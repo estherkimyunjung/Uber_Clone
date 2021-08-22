06becae5cbf81fc84947be86f4692a0b1ec2bf37
estherkimyunjung <estherkimyunjung@gmail.com>
2021-08-21 21:43:39 -0500

    add search option in navigate card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import tw from "tailwind-react-native-classnames";
 
-export default function NavigateCard() {
+const NavigateCard = () => {
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Navigate</Text>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Esther</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
           <GooglePlacesAutocomplete
             placeholder="Where to?"
+            styles={toInputBoxStyles}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
             nearbyPlacesAPI="GooglePlacesSearch"
             debounce={400}
           />
@@ -18,12 +26,23 @@ export default function NavigateCard() {
       </View>
     </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     paddingTop: 20,
     flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
   },
 });
