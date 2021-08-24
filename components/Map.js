bf9c484ec8fa36b783f925f4602d8c952dbc7633
estherkimyunjung <estherkimyunjung@gmail.com>
2021-08-23 22:15:10 -0500

    add drection option

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 import { useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
-import { selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 export default function Map() {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
   return (
     <MapView
       style={tw`flex-1`}
@@ -18,6 +21,15 @@ export default function Map() {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import tw from "tailwind-react-native-classnames";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning, Esther</Text>
@@ -14,6 +20,17 @@ const NavigateCard = () => {
             placeholder="Where to?"
             styles={toInputBoxStyles}
             fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionCard");
+            }}
             enablePoweredByContainer={false}
             query={{
               key: GOOGLE_MAPS_APIKEY,
