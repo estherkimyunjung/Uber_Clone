6f1ab214fef7b1a9a81f24495ff54c66ee53636d
estherkimyunjung <estherkimyunjung@gmail.com>
2021-09-02 23:08:06 -0500

    add destination full image

--- components/Map.js
+++ components/Map.js
@@ -6,12 +6,24 @@ import { useSelector } from "react-redux";
 import tw from "tailwind-react-native-classnames";
 import { selectDestination, selectOrigin } from "../slices/navSlice";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useRef, useEffect } from "react";
 
 export default function Map() {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
+
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -39,6 +51,17 @@ export default function Map() {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
