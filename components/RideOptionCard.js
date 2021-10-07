83404e7d59e7251faf4bf335205e974f613b4b09
estherkimyunjung <estherkimyunjung@gmail.com>
2021-10-06 20:35:34 -0500

    add rideoption components

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,12 +1,67 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
-export default function RideOptionCard() {
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "Uber X",
+    multiplier: 1,
+    image: "http://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "http://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "http://links.papareact.com/7pf",
+  },
+];
+
+const RideOptionCard = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>RideOption Card</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image } }) => (
+          <TouchableOpacity>
+            <Image
+              style={{ width: 100, height: 100, resizeMode: "contain" }}
+              source={{ uri: image }}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default RideOptionCard;
 
 const styles = StyleSheet.create({});
