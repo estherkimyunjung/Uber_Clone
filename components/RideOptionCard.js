a61d765646f43c372b64a6d819e2d0d40f82211e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-10-06 20:54:09 -0500

    ride options design

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FlatList,
   Image,
@@ -35,6 +35,7 @@ const data = [
 
 const RideOptionCard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -49,15 +50,32 @@ const RideOptionCard = () => {
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item: { id, title, multiplier, image } }) => (
-          <TouchableOpacity>
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
             <Image
               style={{ width: 100, height: 100, resizeMode: "contain" }}
               source={{ uri: image }}
             />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>$Cost</Text>
           </TouchableOpacity>
         )}
       />
+      <View>
+        <TouchableOpacity style={tw`bg-black py-3 m-3`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
